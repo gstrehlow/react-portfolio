38c46fd2029032de595b11810ddfa53ac36ebf0a
gstrehlow <90304692+gstrehlow@users.noreply.github.com>
2022-02-12 16:59:12 -0600

    added styling and content to the about section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,31 @@
+import "./styles.css";
+import pfp from "../../assets/images/pfp.jpg";
+
 export default function About() {
   return (
-    <div>
-      <h1>About</h1>
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <div className="card">
+            <div className="card-body">
+              <div className="row">
+                <img className="profile" src={pfp} alt="photograph" />
+              </div>
+            </div>
+            <div className="card-body">
+              <div className="row">
+                <p className="p-about1">Grant Strehlow</p>
+                <p className="p-about">
+                  Software Engineer with a history of working on production web
+                  and mobile applications. Deep understanding in Computer
+                  Science fundamentals, Javascript, ReactJS, NodeJS, MongoDB,
+                  MySQL, CSS, HTML and other technologies.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
